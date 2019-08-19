ca7568668a744edfe3549f2162a2190d0f8335ca
Nadim Farhat <naf34@pitt.edu>
2019-08-19 15:14:59 -0400

    python/DoubleLinkedList updated the class with new functionality                        add comments and docstrings python/test_double_linked_list added test for insert_at_beginning and insert_at_end

--- python/DoubleLinkedList.py
+++ python/DoubleLinkedList.py
@@ -22,119 +22,223 @@ class DoubleLinkedList:
     def 2 : linked list is data structure in which objects are arranged in a linear order, Unlike an array where the order i
     is determined by the indices. The order in a linked list is determeind by a pointer in each object . Linked list a part of dynamic sets and can grow and shrink depending on the need of a program
     The access to a linked list memory is not necessarilly contiguous therefore it might be slower than arrays "Introduction to Algorithms" 3rd Edition"
+    """
 
-
-    def __init__(self,head = None,tail=None):
+    def __init__(self,head = None, tail=None, size = 0):
         """ Initializes an empty list """
         self.head = head
         self.tail = tail
+        self.size = 0
        
 
     def get_head(self):
-        """ Returns the first Node in the linked list """
+        """ Returns the first node/head in the linked list """
         return self.head
     
     def set_head(self,head_ref):
-        """ Sets the head of the linked list to an address/ref. 
+        """ Sets the head of the linked list to a node. 
 
         Keyword arguments:
-        head_ref : reference to the Node Object
+        head_ref : reference to an object of type node
         """
         
         self.head = head_ref
     
     def get_tail(self):
-        ''' Returns the last node in the linked list'''
+        """ Returns the last node in the linked list """
         return self.tail
     
     def set_tail(self,tail_ref):
-        ''' Sets the last node in the linked list to an address/reference'''
+        """ Sets the head of the linked list to a node. 
+
+        Keyword arguments:
+        head_ref : reference to an object of type node
+        """
         self.tail = tail_ref
     
+    def get_size(self):
+        """ Returns the size of the linked list """
+        return self.size
+
+    def __set_size(self,new_size):
+        """ set the size of the linked list """
+        self.size = new_size
+
+    ####################################### Functionality #################################################
     # computational complexity o(1)
 
     def insert_at_beginning(self, item):
+        """ inserts a node containing item before the head.
+            the head will point to the new node.
+
+        Keyword arguments:
+        item : can be of any type 
+        """
+        #  If the list is empty 
         if (self.head == None):
+        # Make is a one node list where head and tail are the same
             new_node  = DoubleNode(item,None,None)
             self.head = new_node
             self.tail = new_node
+            self.size += 1
             return
+        # If the list is not empty 
         new_node = DoubleNode(item,self.head,None)
+        # Inserts before the current head
         self.head.previous = new_node
+        # Assigns head to the new node
         self.head = new_node
+        self.size += 1
         return 
 
     
     # computational complexity o(n)
     def traverse_list(self):
-        """Member function traverse_list
-           starts at the head and continues until it reaches
-           the end of the double linked list and sets the member tail to the last node"""
+        """ Iterates from the begining of the double list till end
+            sets the tail for the linked list """
+
+        # Empty list   
         if self.head == None:
             print("Linked List is empty")
             return None
+        # Starts at the head
         current = self.head
+        # Until we reach the tail, only at tail next == None
         while(current.get_next() != None):
             current = current.get_next()
+        # Sets the tail
         self.tail = current
     
     def reverse_traverse_list(self):
+        """ Iterates from the end of the double list till the begining
+            sets the head for the linked list """
+        # Empty list
         if self.tail == None:
             print("Linked list is empty")
             return None
+        # Starts at the tail
         current = self.tail
+        # Untill we reach the head, only at head previous == None
         while(current.get_previous() != None):
             current = current.get_previous()
+        # Sets the head
         self.head = current
         
     
-    def printList(self):
+    def print_list(self):
+        """ prints the content of the list """
+        # Empty list
         if self.head == None:
             print("Linked List is empty")
             return None
+        # Starts at head
         current = self.head
         print(current.GetItem())
+        # Print all elements until the end
         while(current.get_next() != None):
             current = current.get_next()
             print(current.GetItem())
-    
+
+    def reverse_print_list(self):
+        """ prints the content of the list """
+        # Empty list
+        if self.tail == None:
+            print("Linked List is empty")
+            return None
+        # Starts at tail
+        current = self.tail
+        print(current.GetItem())
+        # Print all elements until the end
+        while(current.get_previous() != None):
+            current = current.get_previous()
+            print(current.GetItem())
+
+
     # computational complexity o(n)
-    def insertAtEnd(self,item):
-        # assuming we dont have the tail
+    def insert_at_end(self,item):
+        """ inserts a node containing item after the tail
+            the tail will point to the new node.
+
+        Keyword arguments:
+        item : can be of any type 
+        """
+        # Empty list
         if self.head == None:
-            self.insertAtBeginning(item)
+            new_node  = DoubleNode(item,None,None)
+            self.head = new_node
+            self.tail = new_node
+            self.size += 1
             return
-        self.traverseList()
+
         node = DoubleNode(item,None,self.tail) # since it is an end node
-        self.tail.SetNext(node)
+        self.tail.set_next(node)
         self.tail = node
+        self.size += 1
         return
 
-     #for insertNode at whe n == pos 
     
-
     def insert(self,pos,item):
+        """ inserts a node containing item before a certain position
+        this function uses the power of double linked list. since we have two pointers we either
+        iterate from the head or tail depending on the size and the position
+        so O(n/2)
+
+        Keyword arguments:
+        pos : a postive integer of the position
+        item : can be of any type 
+
+        """
+        # Empty 
         if  self.head == None:
-            self.insertAtBeginning(item)
-            return None
-        if  pos < 0:
+            print("Empty list")
+            return -1
+        # Invalid poition
+        if  pos < 0 or pos > (self.size -1):
             print("Invalid position")
-            return None
+            return -1
+        # At head
         if  pos == 0:
-            self.insertAtBeginning(item)
-            return
-        new_node = Node(item,None)
-        current = self.head
-        for position in range(pos-1):
-            if  current.get_next().get_next() != None:
+            self.insert_at_beginning(item)
+            # Update the size
+            self.size += 1
+            return 0
+
+        # At tail
+        if pos == self.size -1:
+            self.insert_at_end(item)
+            self.size += 1
+            # Update the size
+            return 0
+        # any other position
+
+        new_node = DoubleNode(item,None,None)
+        # since we are mantaining a head and a tail and the size, at most insert at pos
+        # is n/2
+        # if the position belongs to the first half of the list start from the head 
+
+        if pos < self.size // 2:
+            current = self.head
+            for position in range(pos-1):
                 current = current.get_next()
-            else:
-                print("linked list overflow, return linked list as is ")
-                return -1
-        
-        new_node.SetNext(current.get_next())
-        current.SetNext(new_node)
-        
+            # Insert the node            
+            new_node.set_next(current.get_next())
+            current.get_next().set_previous(new_node)
+            current.set_next(new_node)
+            # Update the size
+            self.size += 1
+        # else the position belong to the second half of the list, start from the tail
+        else:
+            current = self.tail
+            for position in range((self.size - pos)):
+                current = current.get_previous()
+            # Insert the node 
+            new_node.set_previous(current.get_previous())
+            current.get_previous().set_next(new_node)
+            current.set_previous(new_node)
+            #update the size
+            self.size += 1
+             
+
     
     
     def deleteFromBeginning(self):
@@ -149,7 +253,7 @@ class DoubleLinkedList:
         current = self.head
         while(current.get_next().get_next() != None):
             current = current.get_next()
-        current.SetNext(None) 
+        current.set_next(None) 
         self.tail = current
 
     #TODO: check the return    
@@ -169,8 +273,8 @@ class DoubleLinkedList:
                       returning linked list unchanged''')
                 return -1 
         temp = current.get_next()
-        current.SetNext(temp.get_next())
-        temp.SetNext(None)
+        current.set_next(temp.get_next())
+        temp.set_next(None)
         
     def search(self,key): 
         '''A query that given a linked list and a key returns
--- python/test_double_linked_list.py
+++ python/test_double_linked_list.py
@@ -8,11 +8,11 @@ from Node import DoubleNode
 
 
 class TestTraverseList(unittest.TestCase):
-    ''' These sets of function test, test the functionality of 
-    traverse and reverseTraverseList'''
+    """ These sets of function test, test the functionality of 
+    traverse and reverseTraverseList """
 
     def test_empty_dlist(self):
-        # First let us test if we can add a node for an empty list
+        # First let us tests for an empty list
         dList = DoubleLinkedList()
         self.assertEqual(dList.get_head(),None)
         self.assertEqual(dList.get_tail(),None)
@@ -29,7 +29,7 @@ class TestTraverseList(unittest.TestCase):
         dList.traverse_list()
         self.assertEqual(dList.get_head().get_item(),3)
 
-    def test_more_than_one_node_dlist(self):
+    def test_one_or_more_nodes(self):
         # dlist generalize for traverse and reverse traverse for 2 nodes and more
         dNode1 = DoubleNode(1,None,None)
         dNode2 = DoubleNode(2,None,None)
@@ -44,13 +44,70 @@ class TestTraverseList(unittest.TestCase):
         self.assertEqual(dList.get_tail().get_item(),3)
         self.assertEqual(dList.get_head().get_item(),1)
 
+class TestInsertAtBeginning(unittest.TestCase):
+    """ These sets of function test, test the functionality of 
+    traverse and reverseTraverseList """
+    def test_empty_dlist(self):
+    # First let us test if we can add a node for an empty list
+        dList = DoubleLinkedList()
+        dList.insert_at_beginning(4)
+        self.assertEqual(dList.get_tail().get_item(),4)
+        self.assertEqual(dList.get_head().get_item(),4)
+    
+    def test_one_or_more_nodes(self):
+        # test_empty_dlist is the only edge case 
+        dList = DoubleLinkedList()
+        numbers = [19,20,23,100,-100,0]
+        #fills the list 
+        for number in numbers:
+            dList.insert_at_beginning(number)
+        #Tests the list
+        #Insert_at_beginning for Dlist acts like a queue, the first element
+        #Gets pushed till therefore we start comparing from the tail for the dlist
+        #And the beginning for the numbers 
+        node = dList.get_tail()
+        for number in numbers:
+            self.assertEqual(node.get_item(),number)
+            node = node.get_previous()
+
+class TestInsertAtEnd(unittest.TestCase):
+    """ These sets of function test, test the functionality of 
+    traverse and reverseTraverseList """
+    def test_empty_dlist(self):
+    # First let us test if we can add a node for an empty list
+        dList = DoubleLinkedList()
+        dList.insert_at_end(4)
+        self.assertEqual(dList.get_tail().get_item(),4)
+        self.assertEqual(dList.get_head().get_item(),4)
+    
+    def test_one_or_more_nodes(self):
+        # test_empty_dlist is the only edge case 
+        dList = DoubleLinkedList()
+        numbers = [19,20,23,100,-100,0]
+        #fills the list 
+        for number in numbers:
+            dList.insert_at_end(number)
+        # Tests the list
+        # insert_at_end for Dlist acts like a stack, the first element of the list 
+        # gets pushed is the first element of the dlist, is  till therefore we start comparing from the head for the dlist
+        # And the beginning for the numbers 
+        node = dList.get_head()
+        for number in numbers:
+            self.assertEqual(node.get_item(),number)
+            node = node.get_next()
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestTraverseList('test_empty_dlist'))
     suite.addTest(TestTraverseList('test_one_node_dlist'))
-    suite.addTest(TestTraverseList('test_more_than_one_node_dlist'))
+    suite.addTest(TestTraverseList('test_one_or_more_nodes'))
+    suite.addTest(TestInsertAtBeginning('test_empty_dlist'))
+    suite.addTest(TestInsertAtBeginning('test_one_or_more_nodes'))
+    suite.addTest(TestInsertAtEnd('test_empty_dlist'))
+    suite.addTest(TestInsertAtEnd('test_one_or_more_nodes'))
     return suite
-    
+
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
     runner.run(suite())
