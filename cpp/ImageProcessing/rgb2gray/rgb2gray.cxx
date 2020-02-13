120f742cef1585ee822c330f126b9b3c7bd94b8d
Nadim Farhat <naf34@pitt.edu>
2020-02-13 14:27:00 -0500

    rg2bray : Step 1-2 Read rgb image and display it is now working.

--- cpp/ImageProcessing/rgb2gray/rgb2gray.cxx
+++ cpp/ImageProcessing/rgb2gray/rgb2gray.cxx
@@ -1,9 +1,11 @@
 /**
- *     This file does the following
- * 1.  Using VTK read a RGB 2D image from Disk 
+ * This file does the following
+ * 1.  Using ITK reads a RGB 2D image from Disk
+ * 2.  
  * 3.  Creates 3 Buffers ( for R ,G and B) 
- * 4.  Export ImageType(VTK) pixels to R , G and B host 
+ * 4.  Export ImageType(itk) pixels to R , G and B host 
  * 5.  Compute the grey scale according to the formula  L = r*0.21 + g* 0.72 + b * 0.07 
+ * 6.  
  * 7.  Display RGB picture with host grey scale image. 
  * 8.  Create device R, G , B and L 
  * 9.  Move memory from host R , G B to device R G B 
@@ -29,7 +31,6 @@ using output_ImageType = itk::Image<GreyPixelType,DIMENSION>;
 
 using ImageFileReaderType = itk::ImageFileReader<input_ImageType>;
 using ImageFileWriterType = itk::ImageFileWriter<output_ImageType>;
-using ITKToVTKType = itk::ImageTOVTKImageFilter<input_ImageType>:
 
 //using ConvertVTKToITKType = itk::ImageToVTKImageFilter<input_ImageType>;
 
@@ -47,22 +48,36 @@ ImageFileReaderType::Pointer ImageReaderPtr = ImageFileReaderType::New();
 ImageReaderPtr->SetFileName(argv[1]);
 ImageReaderPtr->Update();
 
-ITKToVTKType::Pointer transformerPtr = ITKToVTKType::New();
+input_ImageType::Pointer rgbImage = ImageReaderPtr->GetOutput();
 
-transformerPtr->SetInput(ImageReaderPtr->GetOutput());
+//RescaleFilterType::Pointer rescaleFilterPtr = RescaleFilterType::New();
+
+//rescaleFilterPtr->SetInput(ImageReaderPtr->GetOutput());
+//rescaleFilterPtr->SetOutputMinimum(0);
+//rescaleFilterPtr->SetOutputMaximum(255);
+
+QuickView viewer;
+viewer.AddImage(ImageReaderPtr->GetOutput());
+//viewer.AddImage(RescaleFilterPtr->GetOutput());
+viewer.Visualize();
+/*
+ImageFileWriterType::Pointer ImageWriterPtr = ImageFileWriterType::New();
+
+ImageWriterPtr->SetFileName(argv[2]);
+ImageWriterPtr->SetInput(ImageReaderPtr->GetOutput());
 
 
 try
     {
-        transformerPtr->Update();
+        ImageWriterPtr->Update();
     }
 catch (const itk::ExceptionObject &exp)
     {
-        std::cerr <<"Transforming to vtk error caught";
+        std::cerr <<"Writing image to disk exception caugh";
         std::cerr << exp << std::endl;
         return EXIT_FAILURE;
     }
-vtkImageData * myvtkImageData = filter->GetOutput();
-myvt
+**/
+
 return EXIT_SUCCESS;
 }//end of function main
